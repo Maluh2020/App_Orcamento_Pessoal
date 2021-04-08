dc1662fff78ee1105efe9dd8d4897ac51766bab7
Maluh2020 <maluhmyth@gmail.com>
2021-04-08 19:07:04 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,258 @@
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor){
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+
+	validarDados(){
+		for(let i in this){
+			//console.log(i, this[i])
+			if (this[i] == undefined || this[i] == '' || this[i] == null) {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+//criando classe dinamica
+
+class Bd {
+
+	constructor(){
+		let id = localStorage.getItem('id')
+		if (id === null) {
+			localStorage.setItem('id', 0)
+		}
+	}
+	getProximoId(){
+		let proximoId = localStorage.getItem('id')
+		return parseInt(proximoId) + 1
+	}
+	gravar(d){//recebe um parametro e insere no BD
+		let id = this.getProximoId()
+		localStorage.setItem(id, JSON.stringify(d))
+		localStorage.setItem('id', id)
+
+		/*setItem permite 2 parametros o primeiro é a identificação do objeto 
+	e o segundo é o dado sendo que precisa ser encaminhado via JSON */
+		//precisamos converter o ojbto lietarla em JSON
+		//set inclui um novo item no storage e get recupera
+	}
+
+	recuperarTodosRegistros(){
+		//Array
+		let despesas = Array()
+		let id = localStorage.getItem('id')
+
+		for(let i = 1; i <= id; i++){
+			//JSON.parse converte de string para objeto literal
+			//recuperar todas as despesas cadastradas em localStorage
+			 let despesa = JSON.parse(localStorage.getItem(i))
+
+			 //verificar a possibilidade de haver indices q foram pulados/removidos
+			 //neste caso iremos pular esses indices
+			 if(despesa === null){
+			 	continue
+			 }
+			 //a cada iteração é incluido no array
+			 despesas.push(despesa)
+		}
+
+		return despesas
+	}
+
+	pesquisar(despesa){
+		let despesasFiltradas = Array()
+		despesasFiltradas = this.recuperarTodosRegistros()
+		
+		console.log(despesa)
+		console.log(despesasFiltradas)
+
+		//usando Filter
+		//ano
+		if (despesa.ano != '') {
+			console.log('Filtro de ano')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+		}
+
+		//mes
+		if (despesa.mes != '') {
+			console.log('Filtro de mês')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+		}
+
+		//dia
+		if (despesa.dia != '') {
+			console.log('Filtro de dia')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+		}
+
+		//tipo
+		if (despesa.tipo != '') {
+			console.log('Filtro de tipo')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+		}
+
+		//descrição
+		if (despesa.descricao != '') {
+			console.log('Filtro de descrição')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+		}
+
+		//valor
+		if (despesa.valor != '') {
+			console.log('Filtro de ano')
+			 despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+		}
+
+		return despesasFiltradas
+
+	}
+}
+
+let bd = new Bd()
+
+function cadastrarDespesa(){
+
+	//apos selecionar precisamos criar variáveis para fazer referencia
+
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
+
+//crio um objeto e instancia com a classe Despesa e coloco os valores como parametros
+	let despesa = new Despesa(ano.value, mes.value, dia.value,
+		tipo.value, descricao.value, valor.value
+		)
+
+	if (despesa.validarDados()) {
+		bd.gravar(despesa)
+
+		document.getElementById('modal_titulo').innerHTML = 'Registro inserido com sucesso!!'		
+		document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+		document.getElementById('modal_conteudo').innerHTML = 'Despesa cadastrada com sucesso'
+		document.getElementById('modal_btn').innerHTML = 'Voltar'
+		document.getElementById('modal_btn').className = 'btn btn-success'
+
+		//dialog sucesso
+		$('#modalRegistrarDespesas').modal('show')
+
+		ano.value = ''
+		mes.value = ''
+		dia.value = ''
+		tipo.value = ''
+		descricao.value = ''
+		valor.value = ''
+
+	}else{
+		
+		document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão do registro!!'		
+		document.getElementById('modal_titulo_div').className = 'modal-header text-danger' 
+		document.getElementById('modal_conteudo').innerHTML = 'Erro na gravação, verifique se todoos os campos foram preenchidos corretamente.'
+		document.getElementById('modal_btn').innerHTML = 'Voltar e corrigir'
+		document.getElementById('modal_btn').className = 'btn btn-danger'
+
+		//dialog de erro
+		$('#modalRegistrarDespesas').modal('show')	
+	}
+}
+	
+//  {}	   []
+
+/*Session storage - armazena as informações enqto o browser esta aberto,
+ quando fecha, as informações são perdidas */
+
+ /*Local Storage é um armazenamento persistente, qdo fechamos o browser
+ as informações ficam la ate que as deletemos */
+
+function carregarListaDespesas(){
+
+	let despesas = Array()
+	despesas = bd.recuperarTodosRegistros()
+
+	//selecionando o elemento tbody da tabela
+	let listaDespesas = document.getElementById('listaDespesas')
+
+	//percorer o array despesa listando cada despesa de forma dinamoca
+	despesas.forEach(function(d){
+		
+		//criando linhas tr
+		let linha = listaDespesas.insertRow()
+
+		//criando colunas td
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+    	//ajustar o tipo
+    	switch(d.tipo){
+    		case '1': d.tipo = 'Alimentação'
+    			break
+    		case '2': d.tipo = 'Educação'
+    			break
+    		case '3': d.tipo = 'Lazer'
+    			break
+    		case '4': d.tipo = 'Saúde'
+    			break
+    		case '5': d.tipo = 'Transporte'
+    			break
+    	}
+
+    	linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+	})
+
+}
+
+function pesquisarDespesa(){
+	let ano = document.getElementById('ano').value
+	let mes = document.getElementById('mes').value
+	let dia = document.getElementById('dia').value
+	let tipo = document.getElementById('tipo').value
+	let descricao = document.getElementById('descricao').value
+	let valor = document.getElementById('valor').value
+
+	let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+	let despesas = bd.pesquisar(despesa)
+
+	//selecionando o elemento tbody da tabela
+	let listaDespesas = document.getElementById('listaDespesas')
+	listaDespesas.innerHTML = ''
+
+	//percorrer p array despesas, listando cada despesa de forma dinãmica 
+	despesas.forEach(function(d){
+		
+		//criando linhas tr
+		let linha = listaDespesas.insertRow()
+
+		//criando colunas td
+		linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+    	//ajustar o tipo
+    	switch(d.tipo){
+    		case '1': d.tipo = 'Alimentação'
+    			break
+    		case '2': d.tipo = 'Educação'
+    			break
+    		case '3': d.tipo = 'Lazer'
+    			break
+    		case '4': d.tipo = 'Saúde'
+    			break
+    		case '5': d.tipo = 'Transporte'
+    			break
+    	}
+
+    	linha.insertCell(1).innerHTML = d.tipo
+		linha.insertCell(2).innerHTML = d.descricao
+		linha.insertCell(3).innerHTML = d.valor
+	})
+}
